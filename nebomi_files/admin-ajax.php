02bff7ad7ba4a0833b5767c216a18af06eb35416
Namhyeon, Go <gnh1201@gmail.com>
2020-07-10 18:15:57 +0900

    first commit

---
+++ nebomi_files/admin-ajax.php
@@ -0,0 +1,25 @@
+
+
+.logo img {width:600 !important;height:250 !important;max-height:250 !important;}
+.a-btn, .a-btn-2, .a-btn-3, .a-btn-4, .a-btn-5, .a-btn-6, .a-btn-7, .a-btn-8,
+.btn-style-1 input[type="submit"],
+.btn-style-2 input[type="submit"],
+.btn-style-3 input[type="submit"],
+.btn-style-4 input[type="submit"],
+.btn-style-5 input[type="submit"],
+.btn-style-6 input[type="submit"],
+.btn-style-7 input[type="submit"],
+.btn-style-8 input[type="submit"] {
+}
+
+a {
+}
+
+/*FOOTER*/
+
+#topmenu ul li a {
+}
+
+#topmenu ul ul li a {
+}
+
